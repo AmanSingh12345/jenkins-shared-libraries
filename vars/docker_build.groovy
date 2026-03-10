ccfb876092d781c2f16a3af0ff5e5337560a54ad
AmanSingh12345 <86475645+AmanSingh12345@users.noreply.github.com>
2026-03-10 11:01:39 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def call(String ProjectName, String ImageTag, String DockerHubuser){
-  sh "docker build -t ${DockerHubuser}/${PrjectName}:${ImageTag} ."
+def call(String ProjectName, String ImageTag, String DockerHubUser){
+  sh "docker build -t ${DockerHubUser}/${PrjectName}:${ImageTag} ."
 }
